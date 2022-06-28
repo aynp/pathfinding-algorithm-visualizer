3cf692a8c859768ac75924418eabfe8b0b7a1acb
Prashant <prashant.th02@gmail.com>
2022-06-28 14:35:52 +0530

    Implemented Node Component

--- src/visualizer/node/node.tsx
+++ src/visualizer/node/node.tsx
@@ -1,8 +1,32 @@
-const Node = function () {
+import './node.css';
+
+const Node = function (props: any) {
+  const {
+    row,
+    col,
+    isFinish,
+    isStart,
+    isWall,
+    onMouseDown,
+    onMouseEnter,
+    onMouseUp,
+  } = props;
+
+  const extraClassName = isFinish
+    ? 'node-finish'
+    : isStart
+    ? 'node-start'
+    : isWall
+    ? 'node-wall'
+    : '';
+
   return (
-    <div>
-      <h1>Node</h1>
-    </div>
+    <div
+      id={`node-${row}-${col}`}
+      className={`node ${extraClassName}`}
+      onMouseDown={() => onMouseDown(row, col)}
+      onMouseEnter={() => onMouseEnter(row, col)}
+      onMouseUp={() => onMouseUp(row, col)}></div>
   );
 };
 
