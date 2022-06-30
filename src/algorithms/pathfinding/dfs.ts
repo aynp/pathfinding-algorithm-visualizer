94c467c18eb913d8e044f95933efef0e9385d6c6
aynp <aryan.pathania2003@gmail.com>
2022-06-30 17:52:57 +0530

    Fixed Animation

--- src/algorithms/pathfinding/dfs.ts
+++ src/algorithms/pathfinding/dfs.ts
@@ -13,38 +13,72 @@ const Isvalid = (grid: GridNode[][], row: number, col: number) => {
   return true;
 };
 
+const visorder: GridNode[] = [];
+
 const rec = function (grid: GridNode[][], row: number, col: number) {
   grid[row][col].isVisited = true;
 
-  document.getElementById(`node-${row}-${col}`)?.classList.add('node-visited');
+  visorder.push(grid[row][col]);
 
+  // setTimeout(() => {
   for (let i = 0; i < 4; i++) {
     if (!Isvalid(grid, row + dx[i], col + dy[i])) continue;
     grid[row + dx[i]][col + dy[i]].parent = grid[row][col];
     rec(grid, row + dx[i], col + dy[i]);
   }
+  // }, 100);
 };
 
 const path: GridNode[] = [];
 
 const backtrack = function (start: GridNode, cur: GridNode) {
+  console.log('Running backtrack');
   if (!cur.isVisited) return;
   while (!(cur.row === start.row && cur.col === start.col)) {
     path.unshift(cur);
     cur = cur.parent;
   }
   path.unshift(start);
-  path.forEach(({ row, col }) => {
-    document
-      .getElementById(`node-${row}-${col}`)
-      ?.classList.add('node-shortest');
-  });
 };
 
-const dfs = function (grid: GridNode[][], start: any, end: any) {
+const animateShortest = function (setShowReset: any) {
+  for (let i = 0; i < path.length; i++) {
+    const { row, col } = path[i];
+    setTimeout(() => {
+      document
+        .getElementById(`node-${row}-${col}`)
+        ?.classList.add('node-shortest');
+    }, 10 * i);
+  }
+  setTimeout(() => {
+    setShowReset(true);
+  }, 10 * path.length);
+};
+
+const animateVisited = function (setShowReset: any) {
+  for (let i = 0; i < visorder.length; i++) {
+    const element = visorder[i];
+    setTimeout(() => {
+      document
+        .getElementById(`node-${element.row}-${element.col}`)
+        ?.classList.add('node-visited');
+    }, 10 * i);
+  }
+  setTimeout(() => {
+    animateShortest(setShowReset);
+  }, 10 * visorder.length);
+};
+
+const dfs = function (
+  grid: GridNode[][],
+  start: any,
+  end: any,
+  setShowReset: any
+) {
+  setShowReset(false);
   rec(grid, start.row, start.col);
   backtrack(start, end);
-
+  animateVisited(setShowReset);
   return;
 };
 
